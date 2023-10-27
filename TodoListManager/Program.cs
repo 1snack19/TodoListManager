d7a31da5d2033d9b0d1d6e378e9f881ad4e8fc71
john <john@DESKTOP-OCGKOCE>
2023-10-27 23:25:12 +0700

    Installed a package

--- TodoListManager/Program.cs
+++ TodoListManager/Program.cs
@@ -6,24 +6,65 @@ using System.Threading.Tasks;
 
 namespace TodoListManager
 {
+    
+    enum ConsoleState
+    {
+        Main,
+        Creating,
+    }
+
+
     internal class Program
     {
+
+        static ConsoleState state = ConsoleState.Main;
+        public static void MainUI()
+        {
+            Console.WriteLine("----Main Menu----");
+            Console.Write("Enter an option: ");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "-c":
+                    state = ConsoleState.Creating;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+
+        public static void ReminderCreation() {
+            Console.WriteLine("----Reminder Creation Menu----");
+            Console.Write("Enter an option: ");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "-b":
+                    state = ConsoleState.Main;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("---Todo list manager---");
             bool running = true;
             while (running) {
-                Console.Write("Choose an option: ");
-                string option = Console.ReadLine();
-                switch (option)
+                switch (state)
                 {
-                    case "-q":
-                        Console.WriteLine("Quitting!");
-                        running = false;
+                    case ConsoleState.Main:
+                        MainUI();
+                        break;
+                    case ConsoleState.Creating:
+                        ReminderCreation();
                         break;
                 }
+                Console.Clear();
             }
-
         }
     }
 }
